ab1a5778ffb88effbc8d5f5f3915003a1e7630fb
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-10-15 01:08:51 +0530

    new changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import facebookProvider from "next-auth/providers/facebook"
 
 export default NextAuth({
   // Configure one or more authentication providers
