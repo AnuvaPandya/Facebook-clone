824ae39ea46d9680b2e32ab3b55437b6470a4b04
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-10-16 17:51:39 +0530

    new changes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { getSession } from 'next-auth/client';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Login from '../components/Login';
+import Sidebar from '../components/Sidebar';
 
 export default function Home({ session }) {
   if(!session) return <Login />;
@@ -10,15 +11,16 @@ export default function Home({ session }) {
     <div>
       <Head>
         <title>Facebook</title>
-      </Head>
-      
+      </Head>   
+
       <Header />
 
       <main>
-        {/* Sidebar */}
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
       </main>
+      
     </div>
   )
 }
