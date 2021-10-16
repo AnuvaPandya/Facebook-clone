25662bd48d8d842a7adfc15b654d39dbb0bee6a5
AnuvaPandya <87873813+AnuvaPandya@users.noreply.github.com>
2021-10-16 15:25:59 +0530

    new changes

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,10 @@ import HeaderIcon from './HeaderIcon';
 import { signOut, useSession } from 'next-auth/client';
 
 function Header() {
-    const [session] = useSession(); 
+    const [session] = useSession();
+    const myLoader=({src})=>{
+        return `$/user/photo/${session.user.image}`;
+      } 
 
     return (
         <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg-px-5 shadow-md'>
@@ -36,8 +39,7 @@ function Header() {
             {/* Right */}
             <div className='flex items-center sm:space-x-2 jusify-end'> 
                 {/* Profile picture */}
-                <Image onClick={signOut} className='rounded-full cursor-pointer' src={session.user.image} 
-                width={40} height={40} layout='fixed' />
+                <Image onClick={signOut} className='rounded-full cursor-pointer' src={session.user.image}  loader={myLoader} width={40} height={40} layout='fixed' />
 
                 <p className='whitespace nowrap font-semibold pr-3'>Anuva Pandya</p>
                 <ViewGridIcon className='icon' />
